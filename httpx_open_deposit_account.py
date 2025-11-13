e353b61c3f0e946f7b6ff73ed4506cdcafcb9270
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2025-11-14 00:07:34 +0300

    Практикуемся в работе с HTTPX: получение чека по операции

--- httpx_open_deposit_account.py
+++ httpx_open_deposit_account.py
@@ -1,21 +1,26 @@
-import httpx
 import time
 
+import httpx
+
 create_user_payload = {
-  "email": f"test.{time.time()}@example.com",
-  "lastName": "string",
-  "firstName": "string",
-  "middleName": "string",
-  "phoneNumber": "string"
+    "email": f"user.{time.time()}@example.com",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string",
+    "phoneNumber": "string"
 }
 
-create_user_response =  httpx.post("http://localhost:8003/api/v1/users", json=create_user_payload)
+create_user_response = httpx.post("http://localhost:8003/api/v1/users", json=create_user_payload)
 create_user_response_data = create_user_response.json()
-print("Create user response:",create_user_response_data)
-print("Status code:",create_user_response.status_code)
 
+open_deposit_account_payload = {
+    "userId": create_user_response_data["user"]["id"]
+}
+open_deposit_account_response = httpx.post(
+    "http://localhost:8003/api/v1/accounts/open-deposit-account",
+    json=open_deposit_account_payload
+)
+open_deposit_account_response_data = open_deposit_account_response.json()
 
-create_deposit_account = httpx.post("http://localhost:8003/api/v1/accounts/open-deposit-account",json={"userId": create_user_response_data['user']['id']})
-create_deposit_account_data = create_deposit_account.json()
-print("Create deposit account:", create_deposit_account_data)
-print("Status code:", create_deposit_account.status_code)
+print("Open deposit account response:", open_deposit_account_response_data)
+print("Open deposit account status code:", open_deposit_account_response.status_code)
