fa784fa73e946cee5c8e5da358a626e1d02eb814
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2025-11-14 02:21:38 +0300

    Написание HTTP API клиента

---
+++ httpx_client.py
@@ -0,0 +1,15 @@
+import time
+import httpx
+
+client = httpx.Client(base_url="http://localhost:8003",timeout=5)
+payload = {
+    "email": f"user.{time.time()}@example.com",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string",
+    "phoneNumber": "string"
+}
+
+response = client.post("/api/v1/users",json=payload)
+
+
