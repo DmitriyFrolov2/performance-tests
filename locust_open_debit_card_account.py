307d604a6805b507b61b7d88f96a65ccec202a8c
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2025-11-28 16:33:34 +0300

    Правки по сценарию открытия дебетового счёта

--- locust_open_debit_card_account.py
+++ locust_open_debit_card_account.py
@@ -6,11 +6,9 @@ from tools.fakers import fake
 class OpenDebitCardAccountScenarioUser(HttpUser):
     wait_time = between(1, 3)
 
-
     user_data: dict
 
     def on_start(self) -> None:
-
         request = {
             "email": fake.email(),
             "lastName": fake.last_name(),
@@ -20,15 +18,11 @@ class OpenDebitCardAccountScenarioUser(HttpUser):
         }
         response = self.client.post("/api/v1/users", json=request)
 
-
         self.user_data = response.json()
-
 
     @task
     def open_debit_card_account(self):
-        request_body = {
-        "user_id": self.user_data['user']['id']
-    }
-
-        self.client.post("/api/v1/accounts/open-debit-card-account",json=request_body,
-                         name="/api/v1/accounts/open-debit-card-account")
+        self.client.post(
+            "/api/v1/accounts/open-debit-card-account",
+            json={"userId": self.user_data['user']['id']}
+        )
